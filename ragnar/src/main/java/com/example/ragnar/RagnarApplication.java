989b23088490eb2047ca0ce1112993030740ab2c
Ragnar Kaasik <ragnar.kaasik@gmail.com>
2024-01-04 12:54:49 +0200

    Initial commit

---
+++ ragnar/src/main/java/com/example/ragnar/RagnarApplication.java
@@ -0,0 +1,13 @@
+package com.example.ragnar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagnarApplication.class, args);
+	}
+
+}
---
+++ ragnar/src/test/java/com/example/ragnar/RagnarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ragnar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagnarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
